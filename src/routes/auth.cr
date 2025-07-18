126a535d276c6d938f328e04e6f3617271616704
Alex Abramov <a.abramov@workaround.tech>
2025-07-18 18:59:20 +0300

    transpose users->auth, add login service

--- src/routes/auth.cr
+++ src/routes/auth.cr
@@ -1,8 +1,9 @@
-require "../services/users/register_service"
+require "../services/auth/*"
 
 post "/auth/register" do |env|
   Users::RegisterService.new.call(env)
 end
 
-post "/auth/login" do
+post "/auth/login" do |env|
+  Users::LoginService.new.call(env)
 end
